23eecfcf5773ce393f50f6a19eeddf1980e72dd9
Kirill Semin <sm.ksyomin@yandex.ru>
2023-07-20 22:36:50 +0300

    made changes to locators and image loading

--- src/test/java/com/demoqa/RegistrationFormTests.java
+++ src/test/java/com/demoqa/RegistrationFormTests.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 
@@ -13,15 +15,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTests {
 
+    static {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy="eager";
+        Configuration.baseUrl = "https://demoqa.com/";
     }
 
     @BeforeEach
     void beforeEach() {
-        open("https://demoqa.com/automation-practice-form");
+        open("automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
     }
@@ -43,26 +55,26 @@ public class RegistrationFormTests {
         $("#subjectsInput").setValue("Maths").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/картиночкааа.jpg"));
+        $("#uploadPicture").uploadFromClasspath("картиночкааа.jpg");
         $("#currentAddress").setValue("Emlutina 12, 165");
 
         $("#state").scrollTo();
         $("#state").click();
-        $("#react-select-3-option-0").click();
+        $("#state").$(byText("NCR")).click();
         $("#city").click();
-        $("#react-select-4-option-1").click();
+        $("#city").$(byText("Gurgaon")).click();
 
         $("#submit").click();
 
-        $("tbody tr:nth-child(1) td:nth-child(2)").shouldHave(text("Kirill Ivanov"));
-        $("tbody tr:nth-child(2) td:nth-child(2)").shouldHave(text("kirill.ivanov@gmail.com"));
-        $("tbody tr:nth-child(3) td:nth-child(2)").shouldHave(text("Male"));
-        $("tbody tr:nth-child(4) td:nth-child(2)").shouldHave(text("8980362893"));
-        $("tbody tr:nth-child(5) td:nth-child(2)").shouldHave(text("20 January,1999"));
-        $("tbody tr:nth-child(6) td:nth-child(2)").shouldHave(text("Maths"));
-        $("tbody tr:nth-child(7) td:nth-child(2)").shouldHave(text("Sports, Music"));
-        $("tbody tr:nth-child(8) td:nth-child(2)").shouldHave(text("картиночкааа.jpg"));
-        $("tbody tr:nth-child(9) td:nth-child(2)").shouldHave(text("Emlutina 12, 165"));
-        $("tbody tr:nth-child(10) td:nth-child(2)").shouldHave(text("NCR Gurgaon"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Kirill Ivanov"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("kirill.ivanov@gmail.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("8980362893"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("20 January,1999"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Music"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("картиночкааа.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Emlutina 12, 165"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Gurgaon"));
     }
 }
